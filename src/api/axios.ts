7c8c0ca94e7dc1fc7f0b3deb5608baddda71839f
Sujeevan <sujeevan0024@gmail.com>
2026-02-03 06:03:29 +0530

    backend api url added

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    'https://hello-axis-task-manager-backend-production.up.railway.app/',
 });
 
 // Request Interceptor
